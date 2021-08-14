059909d3b39514e8a971ecb1c8200438c404ecfb
jaga991 <66861775+jaga991@users.noreply.github.com>
2021-08-14 21:05:25 +0800

    finished

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -10,7 +10,7 @@ function PokeCard({
   cardSelected
 }) {
   return(
-    <Card onClick={cardSelected} id={id} className="m-2" style={{ width: '18rem', backgroundColor: selected ? "#4d908e" : "#335c67", border: "2px solid #f4d58d" }}>
+    <Card onClick={cardSelected} id={id} className="m-2" style={{ width: '12rem', backgroundColor: "#708d81"}}>
       <Card.Img variant="top" src={sprite} />
       <Card.Body>
         <Card.Title className="text-center">{name}</Card.Title>
